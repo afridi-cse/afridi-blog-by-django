f0111b01afd86148ed169f57d3c5e3689bd6a4f6
afridi-cse <shahidafridi.cse@gmail.com>
2023-01-29 16:59:03 +0600

    upto user profile update

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+
+
+context = {
+    'posts': Post.objects.all()
+}
+
+def home(request):
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
